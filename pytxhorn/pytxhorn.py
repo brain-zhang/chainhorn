3f6eb88817b9437d4f88078724fbaba41ca2909f
memorybox <memoryboxes@gmail.com>
2019-08-04 11:49:03 +0800

    fix:for openssl gennewkey not thread safe

--- pytxhorn/pytxhorn.py
+++ pytxhorn/pytxhorn.py
@@ -12,6 +12,10 @@ if __name__ == '__main__':
     spv = get_spv_node(settings)
     app = get_app()
     spv.start()
-    app.run(debug=True, use_reloader=False, host=settings.WEB_HOST_IP, port=settings.WEB_HOST_PORT)
+    app.run(debug=True,
+            use_reloader=False,
+            host=settings.WEB_HOST_IP,
+            port=settings.WEB_HOST_PORT,
+            threaded=False)
     signal.signal(signal.SIGINT, shutdown_handler)
     signal.pause()
