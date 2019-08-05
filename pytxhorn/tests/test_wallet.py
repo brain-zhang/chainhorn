b22113cdf0644c4c0f7c18b9a618cf2efc61554c
memorybox <memoryboxes@gmail.com>
2019-08-05 13:48:02 +0800

    issue:#13:add testcase for wallet.get_temp_collections

--- pytxhorn/tests/test_wallet.py
+++ pytxhorn/tests/test_wallet.py
@@ -35,5 +35,21 @@ class WalletTestCase(TestCase):
         private_key_metadata = self.spv.wallet.get('private_key', pk)
         self.logger.info(private_key_metadata)
 
+    def test_get_temp_collections(self):
+        addresses = [
+            'mgSFbgRuX9j1DpDUs2drfvoLd8mUC3G84V',
+            'min4h6ixUBnqDvujUzegc1idZ4nyTwBQfB',
+            'mjxfYJ8uEKfQgRbd1KHgbWudRpo7G9h4A6',
+            'miw2LrQCnm66AiRZ3M45LJt5yZjaxZUGRY',
+            'mm2pTcMUStmM4Wj4DgZTggjAPo8zXS7W1v',
+            'muuaFueV5DhosVrEViH2B6h6XjLgHWkwZt',
+            'n1eKKyX1oY1jEtPvPamcSVHXGGPpkDFXu4',
+            'n1xhAbKKxTGTMMGFUqPqnwDFjSYgdKFPKv'
+        ]
+
+        collections = self.spv.wallet.get_temp_collections()
+        wallet_address = collections['address'].keys()
+        self.assertListEqual(sorted(addresses), sorted(wallet_address))
+
     def tearDown(self):
         super(WalletTestCase, self).tearDown()
