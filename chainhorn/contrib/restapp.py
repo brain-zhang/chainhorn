911611a01d74a80d0c24d40f42374cc9c6f44ce4
brain.zhang <brain.zhangbin@outlook.com>
2019-08-18 10:28:38 +0800

    issue:close:#14

--- chainhorn/contrib/restapp.py
+++ chainhorn/contrib/restapp.py
@@ -15,7 +15,7 @@ from .walletapp import (getinfo,
 
 from flask import Flask
 from flask import request
-from flask_restplus import Api, Resource, reqparse, fields
+from flask_restplus import Api, Resource, fields
 
 
 logger = logging.getLogger('default')
@@ -27,13 +27,11 @@ api = Api(app, version=API_VERSION, title='Chainhorn API',
           description='Chainhorn API',
 )
 
-ns_node = api.namespace('node', description='chainhorn node operations')
-ns_wallet = api.namespace('wallet', description='chainhorn wallet operations')
 
-
-def url_version_wrapper(url):
-    wrapper_url = "/{}{}".format(API_VERSION, url)
-    return wrapper_url
+ns_node = api.namespace('node', path="/{}{}".format(API_VERSION, '/node'),
+                        description='chainhorn node operations')
+ns_wallet = api.namespace('wallet', path="/{}{}".format(API_VERSION, '/wallet'),
+                          description='chainhorn wallet operations')
 
 
 def shutdown_server():
@@ -48,60 +46,76 @@ def shutdown_handler(signal, frame):
     sys.exit(0)
 
 
+@ns_node.route('')
 class NodeGetInfo(Resource):
     @ns_node.doc('get node info')
     def get(self):
+        '''get node info'''
         info = spv.getinfo()
         return {'nodeinfo': info}, 200
 
 
+@ns_node.route('/peers')
 class NodeGetAllPeers(Resource):
     @ns_node.doc('get peers list')
     def get(self):
+        '''get peers list'''
         network_manager = spv.get_network_manager()
         peers = network_manager.get_peers()
         return {'peers': list(peers.keys())}, 200
 
 
+@ns_node.route('/shutdown')
 class NodeShutDown(Resource):
     @ns_node.doc('shutdown node')
     def put(self):
+        '''shutdown node'''
         spv.shutdown()
         return {'shutdown': 'ok'}, 200
 
 
+@ns_node.route('/start')
 class NodeStart(Resource):
     @ns_node.doc('start node')
     def put(self):
+        '''start node'''
         spv.start()
         return {'start': 'ok'}, 200
 
 
+@ns_wallet.route('')
 class WalletGetInfo(Resource):
     @ns_wallet.doc('get wallet info')
     def get(self):
+        '''get wallet info'''
         info = getinfo(spv)
         return {'walletinfo': info}, 200
 
 
+@ns_wallet.route('/broadcast/string:tx')
 class WalletBroadcastTx(Resource):
     @ns_wallet.doc('broadcast raw tx')
     @ns_wallet.param('tx', 'The transaction hash identifier')
     def post(self, tx):
+        '''broadcast raw tx'''
         sendrawtransaction(spv, tx)
         return {'broadcast': 'ok'}, 200
 
 
+@ns_wallet.route('/address')
 class WalletGenNewAddress(Resource):
     @ns_wallet.doc('generate new address')
     def post(self):
+        '''generate new address'''
         new_address = getnewaddress(spv)
         return {'new_address': new_address}, 201
 
 
+@ns_wallet.route('/spends')
 class WalletGetSpends(Resource):
     @ns_wallet.doc('list unspends')
     def get(self):
+        '''list unspends'''
         spents = listspends(spv)
         return spents, 200
 
@@ -112,69 +126,54 @@ sendto_address_model = api.model('SendToAddressParams', {
 })
 
 
+@ns_wallet.route('/sendtoaddress')
 class WalletSendtoAddress(Resource):
     @ns_wallet.doc('send amount to specified address')
-    @ns_wallet.marshal_with(sendto_address_model, code=200)
+    @ns_wallet.expect(sendto_address_model, code=200)
     def post(self):
-        parser = reqparse.RequestParser()
-        parser.add_argument('address', type=str, help='send to address')
-        parser.add_argument('amount', type=str, help='send amount with 1B unit')
-        args = parser.parse_args()
-        address = args['address']
-        amount = args['amount']
+        '''send amount to specified address, exp:send 0.01btc to 1xxxx'''
+        address = api.payload['address']
+        amount = str(api.payload['amount'])
         return sendtoaddress(spv, address, amount), 200
 
 
 sendto_spend_address_model = api.model('SendToSpendAddressParams', {
     'spendhash': fields.String(required=True, description='send to address spendhash'),
     'address': fields.String(required=True, description='send spend to address'),
-    'amount': fields.String(required=True, description='send amount with 1B unit')
+    'amount': fields.Float(required=True, description='send amount with 1B unit')
 })
 
 
+@ns_wallet.route('/sendspendtoaddress')
 class WalletSendSpendtoAddress(Resource):
     @ns_wallet.doc('send amount to specified address from spendhash')
-    @ns_wallet.marshal_with(sendto_spend_address_model, code=200)
+    @ns_wallet.expect(sendto_spend_address_model, code=200)
     def post(self):
-        parser = reqparse.RequestParser()
-        parser.add_argument('spendhash', type=str, help='send to address spendhash')
-        parser.add_argument('address', type=str, help='send spend to address')
-        parser.add_argument('amount', type=str, help='send amount with 1B unit')
-        args = parser.parse_args()
-        spendhash = args['spendhash']
-        address = args['address']
-        amount = args['amount']
+        '''send amount to specified address from spendhash'''
+        spendhash = api.payload['spendhash']
+        address = api.payload['address']
+        amount = str(api.payload['amount'])
         return sendspendtoaddress(spv, spendhash, address, amount), 200
 
 
+@ns_wallet.route('importprivkey/<string:wif>')
 class WalletImportPrivkey(Resource):
     @ns_wallet.doc('import wif private key')
     @ns_wallet.param('wif', 'The wif key')
     def post(self, wif):
+        '''import wif private key'''
         import_address = importprivkey(spv, wif)
-        return import_address, 200
+        return import_address, 201
 
 
+@ns_wallet.route('dumpprivkey/<string:address>')
 class WalletDumpPrivkey(Resource):
     @ns_wallet.doc('dump wif private key by address')
     @ns_wallet.param('address', 'address')
     def get(self, address):
+        '''dump wif private key by address'''
         key = dumpprivkey(spv, address)
         return key, 200
-
-
-api.add_resource(NodeGetInfo, url_version_wrapper('/node'))
-api.add_resource(NodeGetAllPeers, url_version_wrapper('/node/peers'))
-api.add_resource(NodeShutDown, url_version_wrapper('/node/shutdown'))
-api.add_resource(NodeStart, url_version_wrapper('/node/start'))
-api.add_resource(WalletGetInfo, url_version_wrapper('/wallet'))
-api.add_resource(WalletGenNewAddress, url_version_wrapper('/wallet/address'))
-api.add_resource(WalletGetSpends, url_version_wrapper('/wallet/spends'))
-api.add_resource(WalletSendtoAddress, url_version_wrapper('/wallet/sendtoaddress'))
-api.add_resource(WalletSendSpendtoAddress, url_version_wrapper('/wallet/sendspendtoaddress'))
-api.add_resource(WalletImportPrivkey, url_version_wrapper('/wallet/importprivkey/<string:wif>'))
-api.add_resource(WalletDumpPrivkey, url_version_wrapper('/wallet/dumpprivkey/<string:address>'))
-api.add_resource(WalletBroadcastTx, url_version_wrapper('/wallet/broadcasttx/<string:tx>'))
 
 
 def get_app():
